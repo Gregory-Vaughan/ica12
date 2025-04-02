a4ee309c3803d02e049527f914f7662e8ee399bc
Gregory-Vaughan <gpv520@gmail.com>
2025-04-02 19:10:28 -0400

    fiddled with the code, not working yet.

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+
+class Ica12 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory Management App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: InventoryHomePage(),
+    );
+  }
 }
 
-
-
-
-class InventoryApp extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-return MaterialApp(
-title: 'Inventory Management App',
-theme: ThemeData(
-primarySwatch: Colors.blue,
-),
-home: InventoryHomePage(title: 'Inventory Home Page'),
-);
-}
-}
 class InventoryHomePage extends StatefulWidget {
-InventoryHomePage({Key key, this.title}) : super(key: key);
-final String title;
-@override
-_InventoryHomePageState createState() => _InventoryHomePageState();
+  @override
+  _InventoryHomePageState createState() => _InventoryHomePageState();
 }
+
 class _InventoryHomePageState extends State<InventoryHomePage> {
-// TODO: Implement Firestore integration
-@override
-Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(
-title: Text(widget.title),
-),
-body: Center(
-child: Column(
-mainAxisAlignment: MainAxisAlignment.center,
-children: <Widget>[
-Text(
-'Inventory Management System',
-),
-// TODO: Implement inventory list view
-],
-),
-),
-floatingActionButton: FloatingActionButton(
-onPressed: () {
-// TODO: Implement add inventory item
-},
-tooltip: 'Add Item',
-child: Icon(Icons.add),
-),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Inventory Management')),
+      body: Center(child: Text('Firebase Initialized Successfully!')),
+    );
+  }
 }
